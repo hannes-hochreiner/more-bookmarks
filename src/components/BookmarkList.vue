36b2de02f7e6b78b5fb61dae1a712fc5cdcae15a
Hannes Hochreiner <hannes@hochreiner.net>
2019-12-21 20:18:05 +0100

    added more information to group and bookmark lists

--- src/components/BookmarkList.vue
+++ src/components/BookmarkList.vue
@@ -1,9 +1,15 @@
 <template>
   <v-list>
     <v-subheader>Bookmarks</v-subheader>
-    <v-list-item v-for="bookmark in bookmarks" :key="bookmark.id" :href="bookmark.url">
+    <v-list-item
+      two-line
+      v-for="bookmark in bookmarks"
+      :key="bookmark.id"
+      :href="bookmark.url"
+    >
       <v-list-item-content>
         <v-list-item-title v-text="bookmark.name"></v-list-item-title>
+        <v-list-item-subtitle>{{bookmark.url}}</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
   </v-list>
--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -2,12 +2,14 @@
   <v-list>
     <v-subheader>Groups</v-subheader>
     <v-list-item
+      two-line
       v-for="group in groups"
       :key="group.id"
       @click="$emit('selected-group-changed', group)"
     >
       <v-list-item-content>
         <v-list-item-title v-text="group.name"></v-list-item-title>
+        <v-list-item-subtitle>{{group.groupIds.length}} sub-groups {{group.bookmarkIds.length}} bookmarks</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
   </v-list>
