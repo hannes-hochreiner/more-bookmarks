aca6d5544e0953340d9ff566d41544ec7cc92094
Hannes Hochreiner <hannes@hochreiner.net>
2019-12-22 11:58:08 +0100

    switched from text to icons for lists

--- src/components/BookmarkList.vue
+++ src/components/BookmarkList.vue
@@ -1,12 +1,14 @@
 <template>
   <v-list>
-    <v-subheader>Bookmarks</v-subheader>
     <v-list-item
       two-line
       v-for="bookmark in bookmarks"
       :key="bookmark.id"
       :href="bookmark.url"
     >
+      <v-list-item-icon>
+        <v-icon>mdi-bookmark</v-icon>
+      </v-list-item-icon>
       <v-list-item-content>
         <v-list-item-title v-text="bookmark.name"></v-list-item-title>
         <v-list-item-subtitle>{{bookmark.url}}</v-list-item-subtitle>
--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -1,12 +1,14 @@
 <template>
   <v-list>
-    <v-subheader>Groups</v-subheader>
     <v-list-item
       two-line
       v-for="(group, idx) in groups"
       :key="group.id"
       @click="mode == 'view' ? $emit('selected-group-changed', group) : null"
     >
+      <v-list-item-icon>
+        <v-icon>mdi-folder</v-icon>
+      </v-list-item-icon>
       <v-list-item-content>
         <v-list-item-title v-text="group.name"></v-list-item-title>
         <v-list-item-subtitle>{{group.groupIds.length}} sub-groups {{group.bookmarkIds.length}} bookmarks</v-list-item-subtitle>
